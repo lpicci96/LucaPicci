1df9cc082e6a5702c4eb726c64912c4c80dca62e
lpicci96 <lpicci96@gmail.com>
2023-08-09 13:53:21 +0200

    what I do section

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import '../styles/style.css'
 import '../styles/components/header.css'
 import '../styles/components/mobile-nav.css'
 import '../styles/components/hero.css'
+import '../styles/components/skills.css'
 
 import mobileNav from './utils/mobile-nav.js'
 
