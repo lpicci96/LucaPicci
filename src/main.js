7cfc4e179c804a2a58010b614ebf1573681fd560
lpicci96 <lpicci96@gmail.com>
2023-08-11 17:52:42 +0200

    update

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import '../styles/components/mobile-nav.css'
 import '../styles/components/hero.css'
 import '../styles/components/skills.css'
 import '../styles/components/work.css'
+import '../styles/components/about.css'
 
 import mobileNav from './utils/mobile-nav.js'
 
