b821d44576fdfc96001e874f79de56bf595b631b
lpicci96 <lpicci96@gmail.com>
2023-08-11 23:46:58 +0200

    update all

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import '../styles/modern-normalize.css'
 import '../styles/style.css'
+import '../styles/social-icons.css'
+
 import '../styles/components/header.css'
 import '../styles/components/mobile-nav.css'
 import '../styles/components/hero.css'
 import '../styles/components/skills.css'
 import '../styles/components/work.css'
 import '../styles/components/about.css'
+import '../styles/components/footer.css'
 
 import mobileNav from './utils/mobile-nav.js'
 
